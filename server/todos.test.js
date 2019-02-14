8b709c7cb617e17589c82b5e59187f9687728148
Tyler Collier <tyler@intraedge.com>
2019-02-14 02:06:04 -0700

    Remove solution

--- server/todos.test.js
+++ server/todos.test.js
@@ -1,16 +1,8 @@
 const { expect } = require('chai')
 const todos = require('./todos')
-const mongoose = require('mongoose')
 
 describe('todos', () => {
   it('creates a todo with current date', () => {
-    return todos.create({ text: 'Learn promises' })
-      .then(createdTodo => {
-        expect(createdTodo.date).to.exist
-      })
-      .then(() => mongoose.model('Todo').find().exec())
-      .then(records => {
-        expect(records).to.have.length(1)
-      })
+    throw new Error('test not implemented')
   })
 })
